21a2cbd43d13e303d7a246f98767ee88e4fdb5e2
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2024-04-23 13:57:38 +0530

    last fixes

--- emp_details_table.php
+++ emp_details_table.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['sess_user']) || !isset($_SESSION['role']) || $_SESSION['role'] != "mess_member") {
+    header("location:index.php");
+}
+?>
+
+
 <!doctype html>
 <html>
 
